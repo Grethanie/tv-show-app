a7d7c5ad2d17b50fe10b50126422138a50d880e3
Daniel Young <Danyoung91@gmail.com>
2023-03-21 23:36:46 -0400

    AddShow component

--- src/components/AddShows.js
+++ src/components/AddShows.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import { TextField, Paper, Box } from "@mui/material/";
 
 export default function AddShows() {
   return (
-    <div>AddShows</div>
-  )
+    <>
+      <Paper
+        sx={{ padding: "10vh", display: "flex", justifyContent: "center" }}
+      >
+        <h1>Add Show</h1>
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField id="titleField" label="Title" variant="filled" />
+          <TextField id="posterUrl" label="URL of Poster" variant="filled" />
+          <TextField id="genre" label="Genre" variant="filled" />
+          <TextField id="network" label="Creator" variant="filled" />
+          <TextField id="description" label="Description" variant="outlined" />
+        </Box>
+      </Paper>
+    </>
+  );
 }
