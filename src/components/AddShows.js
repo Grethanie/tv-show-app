9d772e475089df74d2acb4d38c8f9c36c556a708
Daniel Young <Danyoung91@gmail.com>
2023-03-24 23:55:08 -0400

    form changes

--- src/components/AddShows.js
+++ src/components/AddShows.js
@@ -1,27 +1,86 @@
-import React from "react";
-import { TextField, Paper, Box } from "@mui/material/";
+import { React, useEffect, useState } from "react";
+import { TextField, Paper, Box, Button } from "@mui/material/";
 
 export default function AddShows() {
+  let { show, setShow } = useState({});
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submitting");
+
+    fetch("http://localhost:4000/shows", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(show),
+    });
+  };
+
+  let handleChange = (e) => {
+    setShow({ ...show, [e.target.name]: e.target.value });
+  };
+
   return (
     <>
       <Paper
         sx={{ padding: "10vh", display: "flex", justifyContent: "center" }}
       >
-        <h1>Add Show</h1>
-        <Box
-          component="form"
-          sx={{
-            "& > :not(style)": { m: 1, width: "25ch" },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField id="titleField" label="Title" variant="filled" />
-          <TextField id="posterUrl" label="URL of Poster" variant="filled" />
-          <TextField id="genre" label="Genre" variant="filled" />
-          <TextField id="network" label="Creator" variant="filled" />
-          <TextField id="description" label="Description" variant="outlined" />
-        </Box>
+        <form onSubmit={handleSubmit}>
+          <Button type="submit" variant="contained">
+            Add Show
+          </Button>
+          <Box
+            sx={{
+              "& > :not(style)": { m: 1, width: "25ch" },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField
+              onChange={handleChange}
+              name="title"
+              id="titleField"
+              aria-label="title"
+              label="Title"
+              variant="filled"
+            />
+            <TextField
+              onChange={handleChange}
+              name="posterUrl"
+              id="posterUrl"
+              aria-label="posterUrl"
+              label="URL of Poster"
+              variant="filled"
+            />
+            <TextField
+              onChange={handleChange}
+              name="genre"
+              id="genre"
+              aria-label="genre"
+              label="Genre"
+              variant="filled"
+            />
+            <TextField
+              onChange={handleChange}
+              name="network"
+              id="network"
+              aria-label="network"
+              label="Creator"
+              variant="filled"
+            />
+            <TextField
+              multiline
+              name="description"
+              rows={4}
+              onChange={handleChange}
+              id="description"
+              aria-label="description"
+              label="Description"
+              variant="outlined"
+            />
+          </Box>
+        </form>
       </Paper>
     </>
   );
