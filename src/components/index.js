2eeeee9b81270112cb7a39692d67490ff14135b3
Daniel Young <Danyoung91@gmail.com>
2023-04-11 16:46:33 -0400

    update imports

--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,4 @@ export { default as Shows } from "./Shows";
 export { default as Show } from "./Show";
 export { default as NotFound } from "./NotFound";
 export { default as ShowCard } from "./ShowCard";
-export { default as AddShows } from "./AddShows";
+export { default as ShowForm } from "./ShowForm";
