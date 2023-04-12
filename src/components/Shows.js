53d8c7274470c3018b28eda10805142b27bd8e6c
Daniel Young <Danyoung91@gmail.com>
2023-04-11 22:09:20 -0400

    tidying up imports

--- src/components/Shows.js
+++ src/components/Shows.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Paper, Grid, Box, Container } from "@mui/material";
-import { Link } from "react-router-dom";
 import ShowCard from "./ShowCard";
 
 export default function Shows() {
