8e3a6c84db4091b84f30ad1e332d01f217292a03
Daniel Young <Danyoung91@gmail.com>
2023-04-11 23:15:44 -0400

    navbar theme fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,29 +5,25 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 
-
 export default function Navbar() {
-
   return (
-    <ThemeProvider theme={theme}>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              TV Tracker
-            </Typography>
-            <Button color="inherit" href="/">
-              Home
-            </Button>
-            <Button color="inherit" href="/Shows">
-              Shows
-            </Button>
-            <Button color="inherit" href="/Shows/Add">
-              Add Shows
-            </Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    </ThemeProvider>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            TV Tracker
+          </Typography>
+          <Button color="inherit" href="/">
+            Home
+          </Button>
+          <Button color="inherit" href="/Shows">
+            Shows
+          </Button>
+          <Button color="inherit" href="/Shows/Add">
+            Add Shows
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
